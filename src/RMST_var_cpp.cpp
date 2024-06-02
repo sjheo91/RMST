f45f16da56ce4f97922e5157e04057af91d4a694
sjheo91 <59630628+sjheo91@users.noreply.github.com>
2024-06-02 21:05:17 +0900

    Update RMST_var_cpp.cpp

--- src/RMST_var_cpp.cpp
+++ src/RMST_var_cpp.cpp
@@ -11,16 +11,16 @@ double RMST_var_cpp(DataFrame data, String method, double tau, double theta=1, i
   int n = data.nrow();
   IntegerVector x = seq(1, n);
   NumericVector rmst;
-  NumericVector rmst_vec = rep(0.0, n_boot);
+  NumericVector rmst_vec = rep(0.0, n_boots);
   
-  NumericVector v = NumericVector(n_boot*n, NumericVector::get_na()); 
-  NumericMatrix ind = NumericMatrix(n_boot, n, v.begin());
+  NumericVector v = NumericVector(n_boots*n, NumericVector::get_na()); 
+  NumericMatrix ind = NumericMatrix(n_boots, n, v.begin());
   
-  for(int i = 0; i < n_boot; ++i){
+  for(int i = 0; i < n_boots; ++i){
     ind(i,_) = sample(as<NumericVector>(x), n, true);
   }
   
-  for(int i = 0; i < n_boot; ++i){
+  for(int i = 0; i < n_boots; ++i){
     if(ensemble==false){
       rmst = RMST_cpp(subset(data, ind(i,_), R_MissingArg), method, tau, theta, family, tol);
     }else{
