0b02cd5ffa2792ffe76b7eb6622bf63f962449a2
sjheo91 <59630628+sjheo91@users.noreply.github.com>
2024-06-02 14:57:46 +0900

    Create RMST_cpp.cpp

---
+++ src/RMST_cpp.cpp
@@ -0,0 +1,33 @@
+#include <Rcpp.h>
+using namespace Rcpp;
+
+// [[Rcpp::export]]
+double RMST_cpp(DataFrame data, String method, double tau, double theta, int family) {
+  
+  double rmst;
+  DataFrame ft;
+  
+  NumericVector time = sort_by(data["time"], data["time"]);
+  NumericVector status = sort_by(data["status"], data["time"]);
+  
+  if(method=="MCG"){
+    ft = MCG_cpp(time, status, theta, family);
+  }else if(method=="SC"){
+    ft = SC_copula_cpp(time, status, theta, family);
+  }else{
+    stop("Check method");
+  }
+  
+  NumericVector t = ft["time"];
+  NumericVector s = ft["surv"];
+  
+  NumericVector t_sub = t[t<tau];
+  NumericVector s_sub = s[t<tau];
+  
+  t_sub.insert(t_sub.begin(), 0);
+  t_sub.insert(t_sub.end(), tau);
+  s_sub.insert(s_sub.begin(), 1);
+  
+  rmst = sum(diff(t_sub)*s_sub);
+  return rmst;
+}
