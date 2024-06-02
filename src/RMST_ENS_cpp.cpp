0b5934623b2862f0fa7b6b53a54e590d977190cc
sjheo91 <59630628+sjheo91@users.noreply.github.com>
2024-06-02 20:15:10 +0900

    Update RMST_ENS_cpp.cpp

--- src/RMST_ENS_cpp.cpp
+++ src/RMST_ENS_cpp.cpp
@@ -7,11 +7,15 @@ double RMST_ENS_cpp(DataFrame data, String method, double tau, NumericVector wei
   Function RMST_cpp( "RMST_cpp" ) ; 
   
   NumericVector rmst = rep(0.0, 3);
+  NumericVector rmst_1 = rep(0.0, 1);
+  NumericVector rmst_2 = rep(0.0, 1);
+  NumericVector rmst_3 = rep(0.0, 1);
   
   if(method=="MCG"){
-    rmst[0] = RMST_cpp(data, method, tau, 2/3, 3, tol)[0];
-    rmst[1] = RMST_cpp(data, method, tau, 2, 3, tol)[0];
-    rmst[2] = RMST_cpp(data, method, tau, 6, 3, tol)[0];
+    rmst_1 = RMST_cpp(data, method, tau, 2/3, 3, tol);
+    rmst_2 = RMST_cpp(data, method, tau, 2, 3, tol);
+    rmst_3 = RMST_cpp(data, method, tau, 6, 3, tol);
+    rmst = (rmst_1, rmst_2, rmst_3);
   }else{
     stop("Check method");
   }
