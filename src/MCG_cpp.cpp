cbe5ade023291f5ba1992129fd38ff7430ce4d45
sjheo91 <59630628+sjheo91@users.noreply.github.com>
2024-06-02 17:48:37 +0900

    Update MCG_cpp.cpp

--- src/MCG_cpp.cpp
+++ src/MCG_cpp.cpp
@@ -1,12 +1,12 @@
 #include <Rcpp.h>
 using namespace Rcpp;
 
-Function MCG_Clayton_cpp( "MCG_Clayton_cpp" ) ; 
-Function MCG_Gumbel_cpp( "MCG_Gumbel_cpp" ) ; 
-Function MCG_Frank_cpp( "MCG_Frank_cpp" ) ; 
-
 // [[Rcpp::export]]
 DataFrame MCG_cpp(NumericVector time, NumericVector status, double theta, int family) {
+
+  Function MCG_Clayton_cpp( "MCG_Clayton_cpp" ) ; 
+  Function MCG_Gumbel_cpp( "MCG_Gumbel_cpp" ) ; 
+  Function MCG_Frank_cpp( "MCG_Frank_cpp" ) ; 
   
   if(family==3){
     DataFrame output = MCG_Clayton_cpp(time, status, theta);
