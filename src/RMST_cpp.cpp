d6c0c91ea6df3650fc1de4f643b968b9e9dcb1f2
sjheo91 <59630628+sjheo91@users.noreply.github.com>
2024-06-03 13:41:16 +0900

    Update RMST_cpp.cpp

--- src/RMST_cpp.cpp
+++ src/RMST_cpp.cpp
@@ -27,7 +27,9 @@ double RMST_cpp(DataFrame data, String method, double tau, int family, double th
   
   NumericVector t_sub = t[t<tau];
   NumericVector s_sub = s[t<tau];
-  
+
+  s_sub = ifelse(s_sub<0, min(s_sub[s_sub>0]), s_sub);
+    
   t_sub.insert(t_sub.begin(), 0);
   t_sub.insert(t_sub.end(), tau);
   s_sub.insert(s_sub.begin(), 1);
